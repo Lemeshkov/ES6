9ecf47862b362907ef61dfb32e4086c5c0da1925
Lemeshkov <evlem3250@gmail.com>
2023-02-28 17:56:23 +0300

    12-19

---
+++ ES6-13.js
@@ -0,0 +1,8 @@
+function removeFirstTwo(list) {
+  // Only change code below this line
+
+  const [a, b, ...arr] = list;
+  // Change this line
+  // Only change code above this line
+  return arr;
+}
